eca64e2f9f33c16590ae0173aa5aa8cef8ec0703
M Sami <beboolean@gmail.com>
2023-09-23 19:56:57 +0530

    Hero Section Added

--- sih-project/src/App.jsx
+++ sih-project/src/App.jsx
@@ -1,6 +1,69 @@
+import "./App.css";
+
+
+
+const colors = ["#000"];
+
+const numBalls = 15;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+  let ball = document.createElement("div");
+  ball.classList.add("ball");
+  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+  ball.style.left = `${Math.floor(Math.random() * 85)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 80)}vh`;
+  ball.style.transform = `scale(${Math.random()})`;
+  ball.style.width = `${Math.random()*3+1}em`;
+  ball.style.height = ball.style.width;
+  
+  balls.push(ball);
+  document.body.append(ball);
+}
+
+// Keyframes
+balls.forEach((el, i, ra) => {
+  let to = {
+    x: Math.random() * (i % 2 === 0 ? -1 : 11),
+    y: Math.random() * 8
+  };
+
+  let anim = el.animate(
+    [
+      { transform: "translate(0, 0)" },
+      { transform: `translate(${to.x}rem, ${to.y}rem)` }
+    ],
+    {
+      duration: (Math.random() + 1) * 2000, // random duration
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out"
+    }
+  );
+});
+
+
+
+
 function App(){
+
+ 
   return (
-    <h1>hello lets start</h1>
+    <>
+    <div className="logo">
+      <a href="/">SIHPREQ</a>
+    </div>
+  <div className="hero-container">
+   <div className="hero-text"><h1>A Story based Quiz</h1> </div>
+
+
+    <a href="/" className="hero-a">LET'S START</a>
+ 
+</div>
+
+
+</>
   )
 }
 
