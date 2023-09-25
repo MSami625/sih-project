c093449083bee8e81f5f4d3932dd9bb0dc83a0ee
M Sami <beboolean@gmail.com>
2023-09-26 03:55:04 +0530

    Story Component made and Details page redesigned

--- sih-project/src/Details.jsx
+++ sih-project/src/Details.jsx
@@ -1,60 +1,113 @@
-import React from "react";
+import React, { useState } from "react";
 import icon from "./assets/open-mind.png";
 import groupicon from "./assets/group.png";
 import "./Details.css";
-function Details(){
-    return (
-           <>
-            <div className="logo">
-                    <img src={icon} width={40} /> <a href="/">SIHPREQ</a>
-                  </div>
-                 <div className="main-container">
-                  <div className="detail-container">
-                  <div className="groupicon">
-                    <img src={groupicon} width={100} /> 
-                    </div>
-                    
-                    <div className="questions">
-                        <h1>What is your name?</h1>
-                    </div>
-                    <div className="inputs">
-                        <input type="Your Name" />
-                    </div>
-                    <div className="questions">
-                        <h1>What is your gender?</h1>
-                    </div>
-                    <div className="inputs">
-                    <select name="Gender" id="gender">
-                    <option value="male">Male</option>
-                    <option value="female">Female</option>
-                    <option value="other">Other</option>
-                    </select>
-                    </div>
-                    <div className="questions">
-                        <h1>What is your age?</h1>
-                    </div>
-                    <div className="inputs">
-                        <input type="number" id="numericInput" min="0" max="100" step="1" inputmode="numeric"/>
-                    </div>
-                    <div className="questions">
-                        <h1>Select Language</h1>
-                    </div>
-                    <div className="inputs">
-                    <select name="Language">
-                    <option value="English">English</option>
-                    <option value="Hindi">Hindi</option>
-                    </select>
-                    </div>
-                    <div className="submit">
-                    <a href="/">Submit</a>
-                  </div>
-                    
+import { useNavigate } from "react-router-dom"; // Import useNavigate
 
-                  </div>
-                  
-                  </div>
-           </>
-    )
+function Details() {
+  const navigate = useNavigate(); // Use useNavigate to get the navigation function
+
+  // Define state variables to store user input
+  const [name, setName] = useState("");
+  const [gender, setGender] = useState("male");
+  const [age, setAge] = useState("");
+  const [language, setLanguage] = useState("English");
+
+  // Handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Create a user object with the input data
+    const user = {
+      name,
+      gender,
+      age,
+      language,
+    };
+
+    // Store user data in local storage as JSON
+    localStorage.setItem("user", JSON.stringify(user));
+
+    // Redirect to the story page using navigate
+    navigate("/story");
+  };
+
+  return (
+    <>
+      <div className="logo">
+        <img src={icon} width={40} /> <a href="/">SIHPREQ</a>
+      </div>
+      <div className="main-container">
+        <div className="detail-container">
+          <div className="groupicon">
+            <img src={groupicon} width={100} />
+          </div>
+          <form onSubmit={handleSubmit}>
+            <div className="questions">
+              <h1>What is your name?</h1>
+            </div>
+            <div className="inputs">
+              <input
+                required
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className="questions">
+              <h1>What is your gender?</h1>
+            </div>
+            <div className="inputs">
+              <select
+                required
+                name="Gender"
+                id="gender"
+                value={gender}
+                onChange={(e) => setGender(e.target.value)}
+              >
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="other">Other</option>
+              </select>
+            </div>
+            <div className="questions">
+              <h1>What is your age?</h1>
+            </div>
+            <div className="inputs">
+              <input
+                required
+                type="number"
+                id="numericInput"
+                min="0"
+                max="100"
+                step="1"
+                inputMode="numeric"
+                value={age}
+                onChange={(e) => setAge(e.target.value)}
+              />
+            </div>
+            <div className="questions">
+              <h1>Select Language</h1>
+            </div>
+            <div className="inputs">
+              <select
+                required
+                name="Language"
+                value={language}
+                onChange={(e) => setLanguage(e.target.value)}
+              >
+                <option value="English">English</option>
+                <option value="Hindi">Hindi</option>
+              </select>
+            </div>
+            <div className="submit">
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Details;
